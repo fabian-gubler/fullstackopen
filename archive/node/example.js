e016474307805d2533aac1049df6a7ae0023c63a
fabian-gubler <fabian.gubler@hotmail.com>
2022-03-10 21:04:53 +0100

    manual update

---
+++ archive/node/example.js
@@ -0,0 +1,12 @@
+let orders = [
+	{ amount: 250},
+	{ amount: 400},
+	{ amount: 100},
+	{ amount: 325},
+]
+
+let totalAmount = orders.reduce(function(sum, order) {
+	return sum + order.amount
+}, 0)
+
+console.log(totalAmount)
